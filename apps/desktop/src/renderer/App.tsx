2a8995e59c04ce26a4fe60cf6e048511b3aa9cc0
Adarsh <adarshnb@gmail.com>
2025-07-19 00:05:12 +0530

    change browserRouter to HashRouter

--- apps/desktop/src/renderer/App.tsx
+++ apps/desktop/src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider, useDispatch } from 'react-redux';
 import { store, initializeWithSampleData } from '@serenity/core';
 import { Layout } from './components/Layout';
